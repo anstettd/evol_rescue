49fbc0937fafbf2a818a90aaed6cf8a40796ddb1
danstett <daniel_anstett@yahoo.ca>
2024-05-23 13:21:03 -0700

    selection vs ppt anom

--- scripts/snp_change/17_selection_clim.R
+++ scripts/snp_change/17_selection_clim.R
@@ -8,6 +8,7 @@
 ###################################################################################
 #Library install and import
 library(tidyverse)
+library(car)
 
 #Import files
 anoms <- read_csv("data/climate_data/climate_anomaly.csv")
@@ -24,8 +25,52 @@ deer_creek_1215 <- deer_creek_timeseries %>% filter(Year>2011) %>% filter(Year<2
 
 #Join data frames
 df1 <- left_join(slope.summary,anoms,by=c("Site"="Paper_ID")) %>% 
-  select(Site,Median,Mean,PPT_wt_1215)
+  select(Site, Latitude,Median,Mean,PPT_wt_1215)
 
 #Imput missing data
+df1$Latitude[10]<-42.27411
 df1$PPT_wt_1215[10]<-mean(deer_creek_1215$PPT_wt.anom)
+
+
+#stats
+lm_mean <- lm(PPT_wt_1215~Mean,data=df1)
+summary(lm_mean)
+Anova(lm_mean,type="III")
+
+lm_Median <- lm(PPT_wt_1215~Median,data=df1)
+summary(lm_Median)
+Anova(lm_Median,type="III")
+
+###################################################################################
+#plot(df1$PPT_wt_1215,df1$Median)
+
+# N-S color gradient
+lat_cols=c("#DC494C","#F88D51","#FDD380","#FEEB9E","#FFFFBF","#D7EF9B","#B2E0A2",
+           "#88CFA4","#5FBAA8","#3F96B7","#4075B4", "#5E4FA2")
+
+
+#Median slope vs. ppt_w
+ggplot(df1, aes(x=Mean, y=PPT_wt_1215)) + 
+  geom_point(aes(fill=as.factor(round(Latitude, 1))),shape=21,size =6)+
+  geom_smooth(method=lm,color="black", lty="dashed", se=FALSE)+
+  scale_y_continuous(name="Winter Precipitation Anomaly")+ #,breaks=c(0.5,1,1.5,2,2.5))+
+  scale_x_continuous(name="Mean Strength of Selection")+ #,breaks=c(-0.1,0,0.1,0.2))+
+  #,breaks=c(0.025,0.03,0.035,0.04,0.045))+
+  scale_fill_manual(values=lat_cols) +
+  theme_classic() + theme(
+    axis.text.x = element_text(size=20, face="bold"),
+    axis.text.y = element_text(size=20,face="bold"),
+    axis.title.x = element_text(color="black", size=24, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=24,vjust = 1.7, face="bold",hjust=0.5),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 14),  # Increase the size of the legend text
+    legend.key.size = unit(2, "lines"),  # Increase the size of the legend dots
+    legend.key.height = unit(1.6, "lines") #Reduce height
+  )
+
+ggsave("Graphs/Selection_climate/1_mean_ppt_wt.pdf",width=8, height = 6, units = "in")
+
+
+
+
 

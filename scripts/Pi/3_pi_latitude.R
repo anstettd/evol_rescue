c6eb250c44cba9c73ceb9c8c1cac29a2335c0602
danstett <daniel_anstett@yahoo.ca>
2024-09-05 13:14:03 -0400

    further pi lat fixes

--- scripts/Pi/3_pi_latitude.R
+++ scripts/Pi/3_pi_latitude.R
@@ -57,10 +57,10 @@ Anova(lm4,type="III")
 
 
 #Pi (Climate-Associated) vs. Latitude
-ggplot(pi_all_pop, aes(x=Lat, y=pi_snp_set,shape=Type)) + 
+ggplot(pi_all_pop, aes(x=Lat, y=pi_snp_set)) + 
   #geom_point(aes(fill=as.factor(round(Lat, 1))),shape=21,size =6)+
-  geom_point(aes(x=Lat,y=pi_snp_set),size =4)+
-  #stat_smooth(method=lm,color="black",formula = y ~ x+I(x^2))+
+  geom_point(aes(shape=Type), size=4) +
+  stat_smooth(method=lm, color="black", formula = y ~ x + I(x^2), aes(group=1)) + 
   scale_y_continuous(name="Pi (Climate Associated)")+
   scale_x_continuous(name="Latitude")+
   #,breaks=c(0.025,0.03,0.035,0.04,0.045))+
@@ -80,10 +80,9 @@ ggsave("Graphs/Pi_latitude/1_lat_pi_snp_set.pdf",width=11, height = 5.5, units =
 
 
 #Pi (Genome-wide) vs. Latitude
-ggplot(pi_all_pop, aes(x=Lat, y=pi_all_snps,shape=Type)) + 
-  #geom_point(aes(fill=as.factor(round(Lat, 1))),shape=21,size =6)+
-  geom_point(aes(x=Lat,y=pi_all_snps),size =4)+
-  #stat_smooth(method =lm,color="black")+
+ggplot(pi_all_pop, aes(x=Lat, y=pi_all_snps)) + 
+  geom_point(aes(shape=Type), size=4) +
+  stat_smooth(method = lm, color = "black", aes(group=1)) +
   scale_y_continuous(name="Pi (Genome-Wide)")+
   scale_x_continuous(name="Latitude")+
   #,breaks=c(0.025,0.03,0.035,0.04,0.045))+
@@ -103,10 +102,9 @@ ggsave("Graphs/Pi_latitude/2_lat_pi_global.pdf",width=11, height = 5.5, units = 
 
 
 #Adaptive vs neutral Pi
-ggplot(pi_all_pop, aes(x=pi_all_snps, y=pi_snp_set,shape=Type)) + 
-  #geom_point(aes(fill=as.factor(round(Lat, 1))),shape=21,size =6)+
-  geom_point(aes(x=pi_all_snps,y=pi_snp_set),size =4)+
-  #stat_smooth(method =lm,color="black")+
+ggplot(pi_all_pop, aes(x = pi_all_snps, y = pi_snp_set)) + 
+  geom_point(aes(shape=Type), size=4) +
+  stat_smooth(method = lm, color = "black", aes(group=1)) +
   scale_y_continuous(name="Pi (Climate Associated)", breaks=c(0, 0.1, 0.2, 0.3, 0.4)) +
   scale_x_continuous(name="Pi (Genome-Wide)", breaks=c(0, 0.1, 0.2, 0.3)) +
   scale_shape_manual(values = c(1,17,18,16))+
@@ -122,30 +120,6 @@ ggplot(pi_all_pop, aes(x=pi_all_snps, y=pi_snp_set,shape=Type)) +
     legend.key.height = unit(1.6, "lines") #Reduce height
   )
 ggsave("Graphs/Pi_latitude/3_pi_vs_pi.pdf",width=11, height = 5.5, units = "in")
-
-
-
-
-#Pi  
-ggplot(pi_all_pop, aes(x=Lat, y=pi_snp_set,fill=Type)) + 
-  #geom_point(aes(fill=as.factor(round(Lat, 1))),shape=21,size =6)+
-  geom_point(aes(x=Lat,y=pi_snp_set),shape=21,size =4)+
-  #stat_smooth(method=lm,color="black",formula = y ~ x+I(x^2))+
-  scale_y_continuous(name="Pi (Climate Associated)")+
-  scale_x_continuous(name="Latitude")+
-  #,breaks=c(0.025,0.03,0.035,0.04,0.045))+
-  scale_fill_manual(values=color.list) +
-  theme_classic() + theme(
-    axis.text.x = element_text(size=20, face="bold"),
-    axis.text.y = element_text(size=20,face="bold"),
-    axis.title.x = element_text(color="black", size=24, vjust = 0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=24,vjust = 1.7, face="bold",hjust=0.5),
-    legend.title = element_blank(),
-    legend.text = element_text(size = 14),  # Increase the size of the legend text
-    legend.key.size = unit(2, "lines"),  # Increase the size of the legend dots
-    legend.key.height = unit(1.6, "lines") #Reduce height
-  )
-ggsave("Graphs/Pi_latitude/legend.pdf",width=6, height = 5.5, units = "in")
 
 
 

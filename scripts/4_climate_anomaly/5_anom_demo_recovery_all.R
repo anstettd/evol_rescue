d4691fa7a3cd8784383388a865b1770010cf71f2
danstett <daniel_anstett@yahoo.ca>
2024-10-21 08:18:13 -0400

    Graph clean up 1

--- scripts/4_climate_anomaly/5_anom_demo_recovery_all.R
+++ scripts/4_climate_anomaly/5_anom_demo_recovery_all.R
@@ -53,7 +53,7 @@ write_csv(recovery_clim_coeff,"data/climate_data/recovery_clim_coeff_alldemo.csv
 
 
 ###################################################################################
-#Population recovery and climate anomalies (no outliers culled)
+#Population recovery and climate anomalies 
 
 demo_pop$Lat.Color<-as.factor(demo_pop$Lat.Color)
 demo_pop$Lat.Color<-factor(demo_pop$Lat.Color,levels=demo_pop$Lat.Color)
@@ -77,27 +77,7 @@ a <- ggplot(demo_pop, aes(x=PPT_wt_1517, y=mean.lambda.recovery)) +
     legend.key.height = unit(1.6, "lines") #Reduce height
   )
 a
-ggsave("Graphs/Climate/Recovery_lamda/1_recovery_lambda_PPT_wt.pdf",width=8, height = 6, units = "in")
-
-b <- ggplot(demo_pop, aes(x=CMD_1517, y=mean.lambda.recovery)) + 
-  geom_smooth(method=lm,color="black", lty="dashed", se=FALSE)+
-  geom_point(aes(fill=demo_pop$Lat.Color), shape=21, size =6)+
-  scale_y_continuous(name="Mean Lambda After Drought")+#, limits=c(0,2.5), breaks=seq(0,2.5,0.5))+
-  scale_x_continuous(name="Climatic Moisure Deficit Anomaly")+
-  #,breaks=c(0.04,0.045,0.05,0.055,0.06))+
-  scale_fill_manual(labels=round(demo_pop$Latitude,1), values=as.character(demo_pop$Lat.Color)) +
-  theme_classic() + theme(
-    axis.text.x = element_text(size=20, face="bold"),
-    axis.text.y = element_text(size=20,face="bold"),
-    axis.title.x = element_text(color="black", size=24, vjust = 0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=24,vjust = 1.7, face="bold",hjust=0.5),
-    legend.title = element_blank(),
-    legend.text = element_text(size = 14),  # Increase the size of the legend text
-    legend.key.size = unit(2, "lines"),  # Increase the size of the legend dots
-    legend.key.height = unit(1.6, "lines") #Reduce height
-  )
-b
-ggsave("Graphs/Climate/Recovery_lamda/2_recovery_lambda_CMD.pdf",width=8, height = 6, units = "in")
+ggsave("Graphs/Climate/2_recovery_lambda_PPT_wt.pdf",width=8, height = 6, units = "in")
 
 
 

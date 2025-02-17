26af616b9543931e12c898b06c8e98ad6aacd663
Amy Angert <angert@mail.ubc.ca>
2025-02-17 14:09:31 -0800

    graphing geometric means for each time period for each population, and comparing adjacent periods with paired t tests

--- scripts/3_demography_scripts/06_geo_means.R
+++ scripts/3_demography_scripts/06_geo_means.R
@@ -2,7 +2,7 @@
 #### PROJECT: Evolutionary rescue of Mimulus cardinalis populations during extreme drought
 #### PURPOSE OF THIS SCRIPT: Make table of Geometric Means
 #### AUTHOR: Daniel Anstett and Amy Angert
-#### DATE LAST MODIFIED: 20241015
+#### DATE LAST MODIFIED: 20250217
 ###################################################################################
 
 # Remove objects and clear workspace
@@ -14,7 +14,30 @@ library(tidyverse)
 #Import demography data (includes metadata)
 demog_recovery <- read_csv("data/demography data/siteYear.lambda_responses_2010-2019.csv")
 
-geo_means <- demog_recovery %>% select(Site,Paper_ID,Latitude,Longitude,mean.lambda.drought,mean.lambda.recovery) %>%
+geo_means <- demog_recovery %>% select(Site,Paper_ID,Latitude,Longitude,mean.lambda.pre,mean.lambda.drought,mean.lambda.recovery) %>%
   arrange(Latitude)
 
 write_csv(geo_means,"data/demography data/geo_means.csv")
+
+# add color ramp column
+color <- read_csv("data/genomic_data/pop_meta_data.csv") %>%
+  dplyr::select(Site, Lat.Color) %>%
+  unique()
+
+# pivot longer for plotting
+geo_means_long <- left_join(geo_means, color) %>% pivot_longer(cols=mean.lambda.pre:mean.lambda.recovery, names_to="time", values_to="lambda")
+
+ggplot(geo_means_long, aes(x=time,y=lambda)) +
+  geom_point(aes(fill=as.factor(Latitude)), shape=21, size=2) +
+  geom_line(aes(group=Latitude), color=geo_means_long$Lat.Color) +
+  scale_color_manual(values=unique(geo_means_long$Lat.Color), aesthetics = c("color", "fill")) +
+  geom_hline(yintercept=1) +
+  theme_classic()
+
+# pre to drought
+t.test(x=geo_means$mean.lambda.pre, y=geo_means$mean.lambda.drought, paired=TRUE)
+
+# drought to recovery
+t.test(x=geo_means$mean.lambda.recovery, y=geo_means$mean.lambda.drought, paired=TRUE)
+
+

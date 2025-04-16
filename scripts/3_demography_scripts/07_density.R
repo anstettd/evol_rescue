163883f91b0defe6d5d346329770ca3a48d3a6bf
Amy Angert <angert@mail.ubc.ca>
2025-04-16 12:43:56 -0700

    adding zeros for year-plots that were sampled but no plants were present; considering different ways to estimate sampled area

--- scripts/3_demography_scripts/07_density.R
+++ scripts/3_demography_scripts/07_density.R
@@ -13,44 +13,105 @@ library(RColorBrewer)
 counts <- read_csv("data/demography data/counts_by_plot.csv") %>% 
   filter(Year>2009) %>% #drop years outside the scope of this analysis
   filter(Year<2020) %>% 
+  filter(PlotID>17) %>% #2 mistaken observations from plots at Carlon that were discarded prior to 2009
   select(-EffectiveTransectLength) #drop unwanted column
 
-### Derive sampled area across all years (J + A + D; exclude E)
+### Derive sampling area from plant coordinates
 plot_area <- counts %>% 
+  filter(Class!="E") %>% filter(Class!="?") %>% # get rid of areas that could not be sorted out, but keep Class=D to encompass sampled areas without living plants
   group_by(PlotID) %>% 
-  filter(Class!="E") %>% 
-  mutate(min_x = min(xmin),
-         max_x = max(xmax),
-         min_y = min(ymin),
-         max_y = max(ymax),
-         max_area = (max_x - min_x)*(max_y - min_y))
+  mutate(min_x_all = min(xmin),
+         max_x_all = max(xmax),
+         min_y_all = min(ymin),
+         max_y_all = max(ymax),
+         max_area_all = (max_x_all - min_x_all)*(max_y_all - min_y_all)) %>% #this is the max extent over which identifiable plants were recorded across all years
+  # using this as denominator assumes that a consistent plot area was scanned yearly, so that years with narrower coordinate extents reflect sparser plants and not decreases in sampling extent
+  # this will underestimate density in years with exclusion zones and when plots are truncated/expanded in different years
+  ungroup() %>% 
+  group_by(PlotID, Year) %>% 
+  mutate(min_x_year = min(xmin),
+         max_x_year = max(xmax),
+         min_y_year = min(ymin),
+         max_y_year = max(ymax),
+         max_area_year = (max_x_year - min_x_year)*(max_y_year - min_y_year)) #this is the max extent over which identifiable plants occurred within a particular year
+  # using this as denominator will reflect local density where plants were definitely sampled, allowing sampled area to wax and wane with exclusion zones
+  # this makes density inestimable when there is only one plant on a plot, and it will overestimate density when there is a small patch of plants in an otherwise empty plot
+ggplot(data=plot_area, aes(x=log(max_area_year), y=log(max_area_all))) + geom_point()
+
+#best solution for exclusion zones would be to calculate their areas and subtract them from sampled area (like swiss cheese)
+# but with this data frame we can only calculate one outer bound of all exclusions, more like a donut than swiss cheese
+#excluded_area <- counts %>% 
+#  filter(Class=="E") %>% 
+#  group_by(PlotID, Year) %>% 
+#  mutate(min_x_E = min(xmin),
+#         max_x_E = max(xmax),
+#         min_y_E = min(ymin),
+#         max_y_E = max(ymax),
+#         max_area_E = (max_x_E - min_x_E)*(max_y_E - min_y_E))
+
+# quick inspect: how different are these?
+ggplot(data=plot_area, aes(x=log(max_area_year), y=log(max_area_all))) + geom_point()
+
 
 density <- plot_area %>% 
   filter(Class=="J"|Class=="A") %>% 
-  select(PlotID, Year, Class, Count, max_area) %>% 
+  select(PlotID, Year, Class, Count, max_area_all, max_area_year) %>% 
   pivot_wider(names_from = Class, values_from = Count) %>% 
-  mutate_at(c(4:5), ~replace(., is.na(.), 0)) %>% 
+  mutate_at(c(4:6), ~replace(., is.na(.), 0)) %>% 
   mutate(N = J + A,
-         Density_J = J/max_area,
-         Density_A = A/max_area,
-         Density_N = N/max_area)
+         Density_J_all = J/max_area_all,
+         Density_A_all = A/max_area_all,
+         Density_N_all = N/max_area_all,
+         Density_J_year = J/max_area_year,
+         Density_A_year = A/max_area_year,
+         Density_N_year = N/max_area_year)
 
 density <- density %>% 
   group_by(PlotID) %>% 
-  mutate(max_dj = max(Density_J),
-         max_da = max(Density_A),
-         max_dn = max(Density_N)) %>% 
+  mutate(max_dj_all = max(Density_J_all),
+         max_da_all = max(Density_A_all),
+         max_dn_all = max(Density_N_all),
+         max_dj_year = max(Density_J_year),
+         max_da_year = max(Density_A_year),
+         max_dn_year = max(Density_N_year)) %>% 
   ungroup() %>% 
-  mutate(RelDens_J = Density_J/max_dj,
-         RelDens_A = Density_A/max_da,
-         RelDens_N = Density_N/max_dn)
+  mutate(RelDens_J_all = Density_J_all/max_dj_all,
+         RelDens_A_all = Density_A_all/max_da_all,
+         RelDens_N_all = Density_N_all/max_dn_all,
+         RelDens_J_year = Density_J_year/max_dj_year,
+         RelDens_A_year = Density_A_year/max_da_year,
+         RelDens_N_year = Density_N_year/max_dn_year) %>% 
+  ungroup() #1117 plot-year combos with non-zero density values
+
+### Read in plot census history so that sampled years with no recorded plants can be recoded as density=0
+census_hist <- read_csv("data/demography data/PlotCensusHistory.csv") %>% 
+  # NA means plot did not exist that year (e.g., not yet created, discarded)
+  # n means plot was skipped that year (e.g., site or plot was inaccessible, triaged for time)
+  # y means site and plot were visited <-- these are the plot-year combinations that should be 0 density if values are not calculable above
+  select(5:15) %>% 
+  pivot_longer(-PlotCode, names_to="Year", values_to="Census") #1600 plot*year combos
+census_hist$Year <- as.numeric(census_hist$Year)
+
+hist_density <- left_join(census_hist, density, by=c("PlotCode"="PlotID", "Year"="Year")) %>% 
+  mutate(
+    across(.cols=Density_J_all:RelDens_N_year,
+           .fns = ~ ifelse(Census=="y" & is.na(max_area_all), 0, .)
+           )
+  )
+
+# quick inspect: how different are these?
+ggplot(data=hist_density, aes(x=Density_J_all, y=Density_J_year)) + geom_point() + geom_abline(slope=1, intercept=0)
+ggplot(data=hist_density, aes(x=Density_A_all, y=Density_A_year)) + geom_point() + geom_abline(slope=1, intercept=0)
+ggplot(data=hist_density, aes(x=Density_N_all, y=Density_N_year)) + geom_point() + geom_abline(slope=1, intercept=0)
+# Pretty darn different. Year-specific areas generate much higher densities than single max area.
+# Though sampling extents do wax and wane a bit from year to year, this is probably less of a factor than plant extents waxing and waning --> use _all instead of _year estimates
 
 ### Join with site metadata
 counts_meta <- read_csv("data/demography data/tidy_counts.csv") %>% #obsolete density estimates; use file only to link PlotIDs to Sites
   select(PlotID, Site) %>% 
   distinct()
   
-density <- left_join(density, counts_meta) %>% 
+density <- left_join(hist_density, counts_meta, by=c("PlotCode"="PlotID")) %>% 
   filter(Site!="MillCreek") %>% #remove unusable sites
   filter(Site!="DeerCreek")
 
@@ -65,8 +126,8 @@ unique(density$Site)
 ### Graphs of density trends over time
 color.list = unique(density$Lat.Color)
 
-# density of all plants
-ggplot(density, aes(x=Year, y=Density_N, color=as.factor(Latitude))) +
+# density of all plants, calculated with area across all sampled years
+ggplot(density, aes(x=Year, y=Density_N_all, color=as.factor(Latitude))) +
   geom_point(alpha=0.3) +
   geom_smooth(method="lm", formula=y~poly(x,2)) +
   ylab("Total plant density") +
@@ -75,7 +136,7 @@ ggplot(density, aes(x=Year, y=Density_N, color=as.factor(Latitude))) +
   theme_classic()
 
 # density of juveniles
-ggplot(density, aes(x=Year, y=Density_J, color=as.factor(Latitude))) +
+ggplot(density, aes(x=Year, y=Density_J_all, color=as.factor(Latitude))) +
   geom_point(alpha=0.3) +
   geom_smooth(method="lm", formula=y~poly(x,2)) +
   ylab("Juvenile plant density") +
@@ -84,7 +145,7 @@ ggplot(density, aes(x=Year, y=Density_J, color=as.factor(Latitude))) +
   theme_classic()
 
 # density of adults
-ggplot(density, aes(x=Year, y=Density_A, color=as.factor(Latitude))) +
+ggplot(density, aes(x=Year, y=Density_A_all, color=as.factor(Latitude))) +
   geom_point(alpha=0.3) +
   geom_smooth(method="lm", formula=y~poly(x,2)) +
   ylab("Adult plant density") +
@@ -93,7 +154,7 @@ ggplot(density, aes(x=Year, y=Density_A, color=as.factor(Latitude))) +
   theme_classic()
 
 # relative density of all plants
-ggplot(density, aes(x=Year, y=RelDens_N, color=as.factor(Latitude))) +
+ggplot(density, aes(x=Year, y=RelDens_N_all, color=as.factor(Latitude))) +
   geom_point(alpha=0.3) +
   geom_smooth(method="lm", formula=y~poly(x,2)) +
   ggpubr::stat_regline_equation(formula=y~poly(x,2), label.x=2010, label.y=0.95, size=2.5) + 
@@ -106,7 +167,7 @@ ggplot(density, aes(x=Year, y=RelDens_N, color=as.factor(Latitude))) +
         legend.title = element_blank())
 
 # relative density of juveniles
-ggplot(density, aes(x=Year, y=RelDens_J, color=as.factor(Latitude))) +
+ggplot(density, aes(x=Year, y=RelDens_J_all, color=as.factor(Latitude))) +
   geom_point(alpha=0.3) +
   geom_smooth(method="glm", method.args = list(family="binomial"), formula=y~poly(x,2)) +
   ggpubr::stat_regline_equation(formula=y~poly(x,2), label.x=2010, label.y=0.95, size=2.5) + 
@@ -119,7 +180,7 @@ ggplot(density, aes(x=Year, y=RelDens_J, color=as.factor(Latitude))) +
         legend.title = element_blank())
 
 # relative density of adults
-ggplot(density, aes(x=Year, y=RelDens_A, color=as.factor(Latitude))) +
+ggplot(density, aes(x=Year, y=RelDens_A_all, color=as.factor(Latitude))) +
   geom_point(alpha=0.3) +
   geom_smooth(method="glm", method.args = list(family="binomial"), formula=y~poly(x,2)) +
   ggpubr::stat_regline_equation(formula=y~poly(x,2), label.x=2010, label.y=0.95, size=2.5) + 
@@ -132,7 +193,7 @@ ggplot(density, aes(x=Year, y=RelDens_A, color=as.factor(Latitude))) +
         legend.title = element_blank())
 
 # classes overlain
-density_tall <- density %>% select(Year, Site, Lat.Color, Latitude, RelDens_J, RelDens_A, RelDens_N) %>%   pivot_longer(cols=5:7, names_to="Class", values_to="RelDens")
+density_tall <- density %>% select(Year, Site, Lat.Color, Latitude, RelDens_J_all, RelDens_A_all, RelDens_N_all) %>%   pivot_longer(cols=5:7, names_to="Class", values_to="RelDens")
 
 ggplot(density_tall, aes(x=Year, y=RelDens, group=Class, color=as.factor(Latitude))) +
   geom_point(alpha=0.3) +

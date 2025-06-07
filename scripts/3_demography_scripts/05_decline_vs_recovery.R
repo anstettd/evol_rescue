519babd7bad3bca312dd6500c60f01364ef18c34
Amy Angert <angert@mail.ubc.ca>
2025-06-07 07:07:43 -0700

    spearman rank correlation of mean r pre and post graph values are correct but color ramp is mismatched to populations

--- scripts/3_demography_scripts/05_decline_vs_recovery.R
+++ scripts/3_demography_scripts/05_decline_vs_recovery.R
@@ -2,7 +2,7 @@
 #### PROJECT: Evolutionary rescue of Mimulus cardinalis populations during extreme drought
 #### PURPOSE OF THIS SCRIPT: Correlate lambda decline vs recovery as a quick check for density dependence
 #### AUTHOR: Daniel Anstett
-#### DATE LAST MODIFIED: 20230815
+#### DATE LAST MODIFIED: 202500606
 ###################################################################################
 
 # Remove objects and clear workspace
@@ -26,45 +26,25 @@ demog_recovery <- read_csv("data/demography data/siteYear.lambda_responses_2010-
 
 ###########################################################################################################
 
-hist(demog_recovery$mean.lambda.drought)
-hist(log(demog_recovery$mean.lambda.drought+0.5))
-hist(demog_recovery$mean.lambda.recovery)
-hist(log(demog_recovery$mean.lambda.recovery+0.5))
+hist(demog_recovery$geomean.l.drought)
+hist(demog_recovery$mean.r.drought)
+hist(demog_recovery$geomean.l.recovery)
+hist(demog_recovery$mean.r.recovery)
 
-cor.test(demog_recovery$mean.lambda.drought, demog_recovery$mean.lambda.recovery, method = 'spearman')
+cor.test(demog_recovery$geomean.l.drought, demog_recovery$geomean.l.recovery, method = 'spearman')
 
-cor.test(log(demog_recovery$mean.lambda.drought+0.5), log(demog_recovery$mean.lambda.recovery+0.5), method = 'spearman')
+cor.test(demog_recovery$mean.r.drought, demog_recovery$mean.r.recovery, method = 'spearman')
 
 demog_recovery$Lat.Color<-as.factor(demog_recovery$Lat.Color)
 demog_recovery$Lat.Color<-factor(demog_recovery$Lat.Color,levels=demog_recovery$Lat.Color)
 
 #Plot lambda decline vs lambda recovery (Fig. S9)
-ggplot(demog_recovery, aes(x=mean.lambda.drought, y=mean.lambda.recovery)) + 
-  geom_point(aes(fill=demog_recovery$Lat.Color), shape=21, size =6)+
-  geom_hline(yintercept = 1, linetype = "dotted", color = "black", size = 0.7) +
-  geom_vline(xintercept = 1, linetype = "dotted", color = "black", size = 0.7) +
-  scale_y_continuous(name="Mean Lambda after Drought")+#, limits=c(-0.3,2.5),breaks=seq(0,2.5,0.5))+
-  scale_x_continuous(name="Mean Lambda during Drought")+
-  #, limits=c(0.2,0.35),breaks=seq(0.1,0.35,0.05))+
-  scale_fill_manual(name = "Latitude (°N)",labels=round(demog_recovery$Latitude,1), values=as.character(demog_recovery$Lat.Color)) +
-  theme_classic() + theme(
-    axis.text.x = element_text(size=20, face="bold"),
-    axis.text.y = element_text(size=20,face="bold"),
-    axis.title.x = element_text(color="black", size=24, vjust = 0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=24,vjust = 1.7, face="bold",hjust=0.5),
-    legend.title = element_text(size = 14, face = "bold"),
-    legend.text = element_text(size = 14),  # Increase the size of the legend text
-    legend.key.size = unit(2, "lines"),  # Increase the size of the legend dots
-    legend.key.height = unit(1.6, "lines") #Reduce hight
-  )
-
-ggplot(demog_recovery, aes(x=log(mean.lambda.drought+0.5), y=log(mean.lambda.recovery+0.5))) + 
-  geom_point(aes(fill=demog_recovery$Lat.Color), shape=21, size =6)+
+ggplot(demog_recovery, aes(x=mean.r.drought, y=mean.r.recovery)) + 
+  geom_point(aes(fill=Lat.Color), shape=21, size =6)+
   geom_hline(yintercept = 0, linetype = "dotted", color = "black", size = 0.7) +
   geom_vline(xintercept = 0, linetype = "dotted", color = "black", size = 0.7) +
-  scale_y_continuous(name="Log Mean Lambda after Drought")+#, limits=c(-0.3,2.5),breaks=seq(0,2.5,0.5))+
-  scale_x_continuous(name="Log Mean Lambda during Drought")+
-  #, limits=c(0.2,0.35),breaks=seq(0.1,0.35,0.05))+
+  scale_y_continuous(name="Mean Pop. Growth after Drought")+
+  scale_x_continuous(name="Mean Pop. Growth during Drought")+
   scale_fill_manual(name = "Latitude (°N)",labels=round(demog_recovery$Latitude,1), values=as.character(demog_recovery$Lat.Color)) +
   theme_classic() + theme(
     axis.text.x = element_text(size=20, face="bold"),
@@ -74,7 +54,8 @@ ggplot(demog_recovery, aes(x=log(mean.lambda.drought+0.5), y=log(mean.lambda.rec
     legend.title = element_text(size = 14, face = "bold"),
     legend.text = element_text(size = 14),  # Increase the size of the legend text
     legend.key.size = unit(2, "lines"),  # Increase the size of the legend dots
-    legend.key.height = unit(1.6, "lines") #Reduce hight
+    legend.key.height = unit(1.6, "lines") #Reduce ehight
   )
+
 ggsave("Graphs/Demography/decline_vs_recovery.pdf",width=8, height = 8, units = "in")
  

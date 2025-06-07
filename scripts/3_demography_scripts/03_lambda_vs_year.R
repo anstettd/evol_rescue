33f9db062176beca0a3d602ad16e4a8180f081d0
Amy Angert <angert@mail.ubc.ca>
2025-06-07 06:56:08 -0700

    comparing distributions of geometric mean lambda and arithmetic mean of r; r has better statistical distributions

--- scripts/3_demography_scripts/03_lambda_vs_year.R
+++ scripts/3_demography_scripts/03_lambda_vs_year.R
@@ -30,18 +30,24 @@ for (i in 1:length(packages_needed)){
 #*******************************************************************************
 dat <- read.csv("data/demography data/siteYear.lambda_2010-2019.csv") %>% select(-Region, -RegionRank) %>% filter(Site!="Mill Creek")
  
+# check distributions
+hist(dat$lambda) #extreme right skew
+hist(log(dat$lambda)) #much better
+
+# express as population growth as intrinsic rate of increase (log lambda)
+dat <- dat %>% mutate(r = log(lambda+0.01)) #adding small value lambdas of 0 don't turn to NA
 
 #*******************************************************************************
 ### 2. Visualize estimates over time for each site
 #*******************************************************************************
 
-ggplot(dat, aes(x=Year, y=lambda)) + 
+ggplot(dat, aes(x=Year, y=r)) + 
   geom_point() +
   geom_smooth(data=filter(dat, Year<2015), method="lm", se=FALSE, col="red") +
   geom_smooth(data=filter(dat, Year>2014), method="lm", se=FALSE, col="blue") +
-  ylab("Lambda") +
+  ylab("Intrinsic rate of increase") +
   #ylim(0,2) +
-  geom_hline(yintercept=1, linetype="dotted") +
+  geom_hline(yintercept=0, linetype="dotted") +
   facet_wrap(~Site, scale="free", nrow=3) +
   theme_classic() #+
   #theme(strip.background = element_blank(), strip.text.x = element_blank(),
@@ -55,7 +61,7 @@ ggplot(dat, aes(x=Year, y=lambda)) +
 
 
 #*******************************************************************************
-### 3A. Calculate mean lambda BEFORE DROUGHT for each site
+### 3A. Calculate geometric mean population growth rate BEFORE DROUGHT for each site
 # Before drought = 2010-11, 2011-12 transitions
 #*******************************************************************************
 
@@ -63,8 +69,10 @@ dat.mean.pre <- dat %>%
   group_by(Latitude, Site) %>%
   filter(Year==2010|Year==2011) %>%
   na.omit() %>%
-  summarize(mean.lambda.pre = exp(mean(log(lambda)))) #GEOMETRIC mean
-
+  dplyr::summarize(mean.r.pre = mean(r), #arithmetic mean of logs
+                   geomean.l.pre = exp(mean(log(lambda)))) #geometric mean of unlogged values
+hist(dat.mean.pre$mean.r.pre) # better 
+hist(dat.mean.pre$geomean.l.pre) #long right tail
 
 #*******************************************************************************
 ### 3B. Calculate mean lambda DURING CORE DROUGHT for each site 
@@ -75,9 +83,10 @@ dat.mean.drought <- dat %>%
   group_by(Latitude, Site) %>% 
   filter(Year==2012|Year==2013|Year==2014) %>% 
   na.omit() %>% 
-  summarize(mean.lambda.drought = exp(mean(log(lambda)))) #GEOMETRIC mean
-
-
+  dplyr::summarize(mean.r.drought = mean(r), 
+                   geomean.l.drought = exp(mean(log(lambda)))) 
+hist(dat.mean.drought$mean.r.drought) #left-skewed
+hist(dat.mean.drought$geomean.l.drought) #better
 #*******************************************************************************
 ### 3C. Calculate mean lambda DURING POST-DROUGHT for each site
 # Post drought = 2015-16, 2016-17, 2017-18 transitions
@@ -87,7 +96,10 @@ dat.mean.recovery <- dat %>%
   group_by(Latitude, Site) %>% 
   filter(Year==2015|Year==2016|Year==2017) %>% 
   na.omit() %>% 
-  summarize(mean.lambda.recovery = exp(mean(log(lambda)))) #GEOMETRIC mean
+  dplyr::summarize(mean.r.recovery = mean(r), 
+            geomean.l.recovery = exp(mean(log(lambda))))
+hist(dat.mean.recovery$mean.r.recovery) #better
+hist(dat.mean.recovery$geomean.l.recovery) #right-skewed
 
 # Add Latitude and other covariates back in
 covar <- read_csv("data/genomic_data/pop_meta_data.csv") %>% 

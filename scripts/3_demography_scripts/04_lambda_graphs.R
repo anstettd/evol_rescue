929e684a948af01733e582ef970275a0ef8be069
Amy Angert <angert@mail.ubc.ca>
2025-06-10 06:22:06 -0700

    redrawing with r instead of lambda

--- scripts/3_demography_scripts/04_lambda_graphs.R
+++ scripts/3_demography_scripts/04_lambda_graphs.R
@@ -1,7 +1,7 @@
 #### PROJECT: Evolutionary rescue of Mimulus cardinalis populations during extreme drought
 #### PURPOSE OF THIS SCRIPT: Produce demography graphs for publication
 #### AUTHOR: Amy Angert & Daniel Anstett
-#### DATE LAST MODIFIED: 20250607
+#### DATE LAST MODIFIED: 20250610
 
 
 #*******************************************************************************
@@ -29,7 +29,10 @@ for (i in 1:length(packages_needed)){
 ### 1. Read in lambda estimates for each site and year 
 #*******************************************************************************
 dat <- read.csv("data/demography data/siteYear.lambda_2010-2019.csv") %>% 
-  mutate(Site.Name=paste(Latitude,Site, sep = "_")) %>% filter(Year!=2018) %>% filter(Site!="Mill Creek")
+  mutate(Site.Name=paste(Latitude,Site, sep = "_")) %>% 
+  mutate(r = log(lambda+0.01)) %>%  #add small value so lambdas=0 doesn't turn to NA)
+  filter(Year!=2018) %>% 
+  filter(Site!="Mill Creek")
 
 # add color ramp column
 color <- read_csv("data/genomic_data/pop_meta_data.csv") %>% 
@@ -49,12 +52,12 @@ dat_decline <- dat %>% filter(Year<2015)
 #*******************************************************************************
 
 #Decline only
-ggplot(dat_decline, aes(x=Year, y=lambda)) +
+ggplot(dat_decline, aes(x=Year, y=r)) +
   geom_point(aes(fill=as.factor(round(Latitude, 1))), shape=21, size=4) +
   geom_smooth(method=lm, se=FALSE, aes(color=as.factor(round(Latitude, 1)))) + 
   scale_color_manual(values=rev(unique(dat_decline$Lat.Color)), aesthetics = c("color", "fill")) +
-  scale_y_continuous(name="Lambda")+ scale_x_continuous(name="Year")+
-  geom_hline(yintercept=1, linetype="dotted") +
+  scale_y_continuous(name="r")+ scale_x_continuous(name="Year")+
+  geom_hline(yintercept=0, linetype="dotted") +
   facet_wrap(~Site.Name, scale="free") + theme_classic() + theme(
     #facet_wrap(~Site.Lat, scale="free") + theme_classic() + theme(
     axis.text.x = element_text(face="bold"),
@@ -70,14 +73,14 @@ ggplot(dat_decline, aes(x=Year, y=lambda)) +
 #*******************************************************************************
 
 #Decline & Recovery
-ggplot(dat, aes(x=Year, y=lambda)) +
+ggplot(dat, aes(x=Year, y=r)) +
   geom_point(data=filter(dat, Year<2012), col="grey") +
   geom_point(data=filter(dat, between(Year,2012,2014)), col="red") +
   geom_point(data=filter(dat, between(Year,2015,2017)), col="blue") +
   geom_point(data=filter(dat, Year>2017), col="grey") +
-  scale_y_continuous(name="Lambda")+ 
+  scale_y_continuous(name="r")+ 
   scale_x_continuous(name="Year",limits = c(2010, 2017))+
-  geom_hline(yintercept=1, linetype="dotted") +
+  geom_hline(yintercept=0, linetype="dotted") +
   facet_wrap(~Site.Name, scale="free") + theme_classic() + theme(
   #facet_wrap(~Site.Lat, scale="free") + theme_classic() + theme(
     axis.text.x = element_text(face="bold"),
@@ -93,16 +96,16 @@ ggsave("Graphs/Demography/01_decline_recovery.pdf",width=14, height = 8, units =
 
 
 #*******************************************************************************
-### 4. Visualize decline over time for select sites (Fig 1D)
+### 4. Visualize decline over time for exemplar site (Fig 1D)
 #*******************************************************************************
 
 #Little Jamison
 dat_little <- dat %>% filter(Site=="Little Jameson Creek") %>% filter(Year<2015)
-plot_3 <- ggplot(dat_little, aes(x=Year, y=lambda)) + geom_point(size=4,shape=21,fill="#9BD7A4") +
+plot_3 <- ggplot(dat_little, aes(x=Year, y=r)) + geom_point(size=4,shape=21,fill="#9BD7A4") +
   #geom_smooth(data=filter(dat_little, Year<2015), method="lm", se=FALSE, col="#9BD7A4",size=1.5) +
   #geom_smooth(data=filter(dat_little, Year>2014), method="lm", se=FALSE, col="blue") +
-  scale_y_continuous(name="Lambda")+ scale_x_continuous(name="")+
-  geom_hline(yintercept=1, linetype="dotted",size=0.8) + theme_classic() + theme(
+  scale_y_continuous(name="r")+ scale_x_continuous(name="")+
+  geom_hline(yintercept=0, linetype="dotted",size=0.8) + theme_classic() + theme(
     #facet_wrap(~Site.Lat, scale="free") + theme_classic() + theme(
     axis.text.x = element_text(size=18,face="bold"),
     axis.text.y = element_text(size=18,face="bold"),
@@ -111,65 +114,5 @@ plot_3 <- ggplot(dat_little, aes(x=Year, y=lambda)) + geom_point(size=4,shape=21
 plot_3
 ggsave("Graphs/Demography/sites/05_little_jamison.pdf",width=5, height = 4, units = "in")
 
-
-#*******************************************************************************
-### 5. Visualize estimates over time for select sites
-#*******************************************************************************
-
-#Kitchen creek
-dat_kitchen <- dat %>% filter(Paper_ID==15)
-plot_1 <- ggplot(dat_kitchen, aes(x=Year, y=lambda)) + geom_point(size=2) +
-  geom_smooth(data=filter(dat_kitchen, Year<2015), method="lm", se=FALSE, col="red") +
-  geom_smooth(data=filter(dat_kitchen, Year>2014), method="lm", se=FALSE, col="blue") +
-  scale_y_continuous(name="Lambda")+ scale_x_continuous(name="")+
-  geom_hline(yintercept=1, linetype="dotted") + theme_classic() + theme(
-    #facet_wrap(~Site.Lat, scale="free") + theme_classic() + theme(
-    axis.text.x = element_text(size=18,face="bold"),
-    axis.text.y = element_text(size=18,face="bold"),
-    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=22,vjust = 2, face="bold",hjust=0.5))
-ggsave("Graphs/Demography/sites/01_kitchen.pdf",width=6, height = 4, units = "in")
-
-#Wawona
-dat_Wawona <- dat %>% filter(Paper_ID==5)
-plot_2 <- ggplot(dat_Wawona, aes(x=Year, y=lambda)) + geom_point(size=2) +
-  geom_smooth(data=filter(dat_Wawona, Year<2015), method="lm", se=FALSE, col="red") +
-  geom_smooth(data=filter(dat_Wawona, Year>2014), method="lm", se=FALSE, col="blue") +
-  scale_y_continuous(name="Lambda")+ scale_x_continuous(name="")+
-  geom_hline(yintercept=1, linetype="dotted") + theme_classic() + theme(
-    #facet_wrap(~Site.Lat, scale="free") + theme_classic() + theme(
-    axis.text.x = element_text(size=18,face="bold"),
-    axis.text.y = element_text(size=18,face="bold"),
-    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=22,vjust = 2, face="bold",hjust=0.5))
-ggsave("Graphs/Demography/sites/02_wawona.pdf",width=6, height = 4, units = "in")
-
-#Little Jamison
-dat_little <- dat %>% filter(Paper_ID==7)
-plot_3 <- ggplot(dat_little, aes(x=Year, y=lambda)) + geom_point(size=2) +
-  geom_smooth(data=filter(dat_little, Year<2015), method="lm", se=FALSE, col="red") +
-  geom_smooth(data=filter(dat_little, Year>2014), method="lm", se=FALSE, col="blue") +
-  scale_y_continuous(name="Lambda")+ scale_x_continuous(name="")+
-  geom_hline(yintercept=1, linetype="dotted") + theme_classic() + theme(
-    #facet_wrap(~Site.Lat, scale="free") + theme_classic() + theme(
-    axis.text.x = element_text(size=18,face="bold"),
-    axis.text.y = element_text(size=18,face="bold"),
-    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=22,vjust = 2, face="bold",hjust=0.5))
-ggsave("Graphs/Demography/sites/03_little_jamison.pdf",width=6, height = 4, units = "in")
-
-#Coast Fork of Willamette
-dat_coast <- dat %>% filter(Paper_ID==55)
-plot_4 <- ggplot(dat_coast, aes(x=Year, y=lambda)) + geom_point(size=2) +
-  geom_smooth(data=filter(dat_coast, Year<2015), method="lm", se=FALSE, col="red") +
-  geom_smooth(data=filter(dat_coast, Year>2014), method="lm", se=FALSE, col="blue") +
-  scale_y_continuous(name="Lambda")+ scale_x_continuous(name="")+
-  geom_hline(yintercept=1, linetype="dotted") + theme_classic() + theme(
-    #facet_wrap(~Site.Lat, scale="free") + theme_classic() + theme(
-    axis.text.x = element_text(size=18,face="bold"),
-    axis.text.y = element_text(size=18,face="bold"),
-    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=22,vjust = 2, face="bold",hjust=0.5))
-ggsave("Graphs/Demography/sites/04_coast_fork.pdf",width=6, height = 4, units = "in")
 
 

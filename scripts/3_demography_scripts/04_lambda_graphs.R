167c53739f14a1d85d6ce4aeaa905115cb4e9923
Amy Angert <angert@mail.ubc.ca>
2025-12-11 14:14:01 -0800

    deleting faceted plots of all three time periods

--- scripts/3_demography_scripts/04_lambda_graphs.R
+++ scripts/3_demography_scripts/04_lambda_graphs.R
@@ -93,34 +93,6 @@ ggplot(r_long, aes(x=factor(time, level=level_order_all), y=mean_r, group=Latitu
     fill  = guide_legend(reverse = TRUE))
 ggsave("Graphs/Demography/rmeans_raw_decline_recovery_unfaceted.pdf",width=8, height = 6.5, units = "in")
 
-# All three time periods on same graph, raw values, faceted by population
-ggplot(r_long, aes(x=factor(time, level=level_order_all), y=mean_r, group=Latitude, fill=as.factor(Latitude))) +
-  facet_wrap(~Latitude, scale="free") +
-  geom_line(aes(colour=as.factor(Latitude)), position=dodge, size=1.5) +
-  geom_point(shape=21, size=3, position=dodge) +
-  geom_errorbar(aes(ymin=ymin, ymax=ymax, colour=as.factor(Latitude)), width=0.1, position=dodge) +
-  scale_fill_manual(values=colours_in_to_plot$Lat.Color, 
-                    labels = unique(r_long$Paper_ID) ) +
-  scale_color_manual(values=colours_in_to_plot$Lat.Color, 
-                     labels = unique(r_long$Paper_ID )) +
-  #scale_color_manual(values=unique(r_long$Lat.Color), aesthetics=c("color", "fill")) +
-  scale_y_continuous(name="Mean Population Growth Rate")+ 
-  scale_x_discrete(name="Time Period", labels=c("Pre", "Dro", "Rec")) + 
-  geom_hline(yintercept=0) +
-  theme_classic() + theme(
-    axis.text.x=element_text(size=16,face="bold"),
-    axis.text.y=element_text(size=16,face="bold"),
-    axis.title.x=element_text(color="black", size=20, vjust=0.5, face="bold"),
-    axis.title.y=element_text(color="black", size=20,vjust=2, face="bold", hjust=0.5),
-    strip.background=element_blank(), 
-    strip.text.x=element_blank(),
-    legend.text = element_text(size = 14),
-    legend.key.size = unit(2, "lines"),
-    legend.key.height = unit(1.6, "lines"), #Reduce height
-    legend.title=element_blank())+
-  guides(color = guide_legend(reverse = TRUE, override.aes = list(linetype = 0)),
-         fill  = guide_legend(reverse = TRUE))
-ggsave("Graphs/Demography/rmeans_raw_decline_recovery_faceted.pdf",width=8, height = 6.5, units = "in")
 
 
 # Pre-drought to drought period only, raw values, unfaceted (FIGURE 1D)
@@ -248,31 +220,7 @@ c <- ggplot(filter(r_means_norm_long, Site!="South Fork Middle Fork Tule"), aes(
 c
 ggsave("Graphs/Demography/rmeans_norm_decline_recovery_unfaceted.pdf",width=5, height=4, units="in")
 
-# Normalized trajectories (all relative to starting lambda pre-drought), faceted by population 
-d <- ggplot(filter(r_means_norm_long, Site!="South Fork Middle Fork Tule"), aes(x=factor(time, level=level_order_all), y=mean_r_norm, group=Latitude, fill=as.factor(Latitude))) +
-  facet_wrap(~Latitude, scale="free")+
-  geom_point(shape=21, size=3, position=dodge) +
-  #geom_errorbar(aes(ymin=ymin, ymax=ymax, colour=as.factor(Latitude)), width=0.1, position=dodge) +
-  geom_line(aes(colour=as.factor(Latitude)), position=dodge, size=1.5) +
-  scale_fill_manual(values=colours_in_to_plot$Lat.Color, 
-                    labels = unique(r_means_norm_long$Paper_ID) ) +
-  scale_color_manual(values=colours_in_to_plot$Lat.Color, 
-                     labels = unique(r_means_norm_long$Paper_ID )) +
-  scale_y_continuous(name="Normalized r")+ 
-  scale_x_discrete(name="Time Period", labels=c("Pre", "Drght", "Recov")) + 
-  geom_hline(yintercept=1) +
-  theme_classic() + theme(
-    axis.text.x = element_text(face="bold"),
-    axis.text.y = element_text(size=11, face="bold"),
-    axis.title.x = element_text(color="black", size=20, vjust=0.5, face="bold"),
-    axis.title.y = element_text(color="black", size=20,vjust=2, face="bold", hjust=0.5),
-    strip.background=element_blank(), 
-    strip.text.x=element_blank(),
-    legend.title=element_blank())+
-  guides(color = guide_legend(reverse = TRUE, override.aes = list(linetype = 0)),
-         fill  = guide_legend(reverse = TRUE))
-d
-ggsave("Graphs/Demography/rmeans_norm_decline_recovery_faceted.pdf",width=5, height=4, units="in")
+
 
 
 
